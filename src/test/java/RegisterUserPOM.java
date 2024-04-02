046f0224e2a311f02d42c0aaf213504194b25a5d
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-04-02 13:12:32 +0300

    Updated data provider

--- src/test/java/RegisterUserPOM.java
+++ src/test/java/RegisterUserPOM.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -27,15 +26,8 @@ public class RegisterUserPOM {
         webDriver.close();
     }
 
-    @DataProvider(name = "getUser")
-    public Object [][] getUsers(){
-        return new Object[][]{
-                {"Natalia", "natalia@obv.bg", "12.12.1221", "Password12", "Password12", "Very cool person"}
-        };
-    }
-
-    @Test(dataProvider = "getUser")
-    public void registerUser(String username, String email, String birthdate, String password, String confirmpassword, String publicinfo){
+    @Test
+    public void registerUser(){
         Header header = new Header(webDriver);
         HomePage homePage = new HomePage(webDriver);
         LoginPage loginPage = new LoginPage(webDriver);
@@ -51,12 +43,12 @@ public class RegisterUserPOM {
         loginPage.clickRegister();
         Assert.assertTrue(registerPage.isUrlLoaded(), "Current page is not register");
 
-        registerPage.fillUserName(username);
-        registerPage.fillEmail(email);
-        registerPage.fillBirthDate(birthdate);
-        registerPage.fillPassword(password);
-        registerPage.fillConfirmPassword(confirmpassword);
-        registerPage.fillPublicInfo(publicinfo);
+        registerPage.fillUserName(DataProvider.generateRandomUsername());
+        registerPage.fillEmail(DataProvider.generateRandomEmail());
+        registerPage.fillBirthDate(DataProvider.generateRandomDOB());
+        registerPage.fillPassword(DataProvider.generateRandomPassword());
+        registerPage.fillConfirmPassword(DataProvider.generateRandomConfirmPassword());
+        registerPage.fillPublicInfo(DataProvider.generateRandomInfo());
 
         registerPage.clickSignIn();
 
